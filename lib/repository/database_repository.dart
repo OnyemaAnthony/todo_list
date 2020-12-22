a428af4b3d39a7bc12dc2de52d572df6091d9531
anthonychibuike <anthonychibuike246@gmail.com>
2020-12-22 21:10:42 +0100

    finish db repo

--- lib/repository/database_repository.dart
+++ lib/repository/database_repository.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
@@ -7,11 +8,9 @@ import 'package:todo_list/models/todo_list_model.dart';
 import 'package:todo_list/utility/utilities.dart';
 
 class DatabaseRepository {
-
   static final DatabaseRepository _instance = DatabaseRepository.internal();
 
-  factory DatabaseRepository()=> _instance;
-
+  factory DatabaseRepository() => _instance;
 
   static Database _db;
 
@@ -34,14 +33,60 @@ class DatabaseRepository {
 
   void _onCreate(Database db, int newVersion) async {
     await db.execute(
-        "CREATE TABLE ${Utility.todoTable}(${Utility
-            .id} INTEGER PRIMARY KEY, ${Utility.task} TEXT,${Utility
-            .deadline }TEXT})");
-
+        "CREATE TABLE ${Utility.todoTable}(${Utility.id} INTEGER PRIMARY KEY, ${Utility.task} TEXT,${Utility.deadline}TEXT})");
   }
 
-  Future<int> saveTodo(TodoListModel todoListModel){
+  Future<int> saveTask(TodoListModel todoListModel) async {
+    var dbClient = await db;
 
+    int result =
+        await dbClient.insert(Utility.todoTable, todoListModel.toMap());
+    return result;
   }
 
+  Future<List> geAllTask() async {
+    var dbClient = await db;
+
+    var result = await dbClient.rawQuery("SELECT * FROM ${Utility.todoTable}");
+
+    return result.toList();
+  }
+
+  Future<int> getCount() async {
+    var dbClient = await db;
+
+    return Sqflite.firstIntValue(
+        await dbClient.rawQuery("SELECT COUNT(*) FROM ${Utility.todoTable}"));
+  }
+
+  Future<TodoListModel> getTask(int id) async {
+    var dbClient = await db;
+
+    var result = await dbClient.rawQuery(
+        "SELECT * FROM ${Utility.todoTable} WHERE ${Utility.id} = $id");
+
+    if (result.length == 0) {
+      return null;
+    } else {
+      return TodoListModel.fromMap(result.first);
+    }
+  }
+
+  Future<int> deleteTask(int id) async {
+    var dbClient = await db;
+
+    return await dbClient
+        .delete(Utility.todoTable, where: "${Utility.id} = ?", whereArgs: [id]);
+  }
+
+  Future<int> updateTask(TodoListModel todoListModel) async {
+    var dbClient = await db;
+    return await dbClient.update(Utility.todoTable, todoListModel.toMap(),
+        where: "${Utility.id} = ?", whereArgs: [todoListModel.id]);
+  }
+
+  Future close()async{
+    var dbClient = await db;
+    return dbClient.close();
+  }
 }
