4b6311ae7ef6664e15fcc64d65a6dbbda8508e11
engels <engelmmanuel@gmail.com>
2020-12-23 15:52:15 +0100

    Added floating action button

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -6,12 +6,24 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  anthony(){
+    print('Hello World');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Todo List'),
         centerTitle: true,
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.blue,
+        child: Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
+        onPressed: anthony,
       ),
     );
   }
