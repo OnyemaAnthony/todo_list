1fce24e9153bb0cbb8cc613abe62a45cf07b03fd
anthonychibuike <anthonychibuike246@gmail.com>
2020-12-24 00:05:32 +0100

    finish add task screen

--- lib/screens/add_task_screen.dart
+++ lib/screens/add_task_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class AddTaskScreen extends StatefulWidget {
   @override
@@ -6,6 +7,11 @@ class AddTaskScreen extends StatefulWidget {
 }
 
 class _AddTaskScreenState extends State<AddTaskScreen> {
+  TextEditingController taskController = TextEditingController();
+  TextEditingController dateController = TextEditingController();
+  TextEditingController timeController = TextEditingController();
+  final DateFormat formatter = DateFormat('dd, MMMM, yyyy');
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,18 +20,20 @@ class _AddTaskScreenState extends State<AddTaskScreen> {
         title: Text('Add a new Task'),
       ),
       body: Container(
-        margin: EdgeInsets.only(top: 20),
+        margin: EdgeInsets.all(20),
         child: Column(
-          //mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+
             Text("What need to be done?"),
-            gi
+
             Row(
               children: [
                 Expanded(
                   child: Container(
                     width: 400,
                     child: TextFormField(
+                      controller: taskController,
                       textInputAction: TextInputAction.newline,
                       keyboardType: TextInputType.multiline,
                       maxLines: null,
@@ -37,12 +45,105 @@ class _AddTaskScreenState extends State<AddTaskScreen> {
                   ),
                 ),
                 SizedBox(width: 10,),
-                Icon(Icons.keyboard_voice)
+                Icon(Icons.keyboard_voice,color: Theme.of(context).primaryColor,size: 28,),
+              ],
+            ),
+
+           SizedBox(height: 40,),
+            Center(child: Text("Dead line",style:TextStyle(
+              fontSize: 19
+            ),)),
+            SizedBox(height: 10,),
+
+            Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    width: 400,
+                    child: TextFormField(
+
+                      onTap: (){
+                        _showDatePicker(context);
+                      },
+                      controller: dateController,
+                      textInputAction: TextInputAction.newline,
+                      keyboardType: TextInputType.multiline,
+                      maxLines: null,
+                      decoration: InputDecoration(
+                        hintText:'Enter Date',
+                        //border: OutlineInputBorder(),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(width: 10,),
+
+                Icon(Icons.date_range,color: Theme.of(context).primaryColor,size: 28,),
+              //  SizedBox(height: 10,),
+
+               dateController.text.isEmpty ? Container():  Expanded(child: buildTime()),
+
               ],
             ),
           ],
         ),
       ),
     );
+  }
+
+  Row buildTime(){
+    return Row(
+      children: [
+        Expanded(
+          child: Container(
+            width: 400,
+            child: TextFormField(
+
+              onTap: (){
+                _selectTime(context);
+              },
+              controller: timeController,
+              textInputAction: TextInputAction.newline,
+              keyboardType: TextInputType.multiline,
+              decoration: InputDecoration(
+                hintText:'Enter Time',
+                //border: OutlineInputBorder(),
+              ),
+            ),
+          ),
+        ),
+        SizedBox(width: 10,),
+
+        Icon(Icons.timer,color: Theme.of(context).primaryColor,size: 28,),
+      ],
+    );
+  }
+
+  Future<Null> _selectTime(BuildContext context) async {
+    final TimeOfDay picked = await showTimePicker(
+      context: context,
+      initialTime: TimeOfDay.now(),
+    );
+    if (picked != null)
+      setState(() {
+      timeController.text = picked.toString().substring(10,15);
+      });
+  }
+
+  void _showDatePicker(BuildContext context) {
+    showDatePicker(
+        context: context,
+        initialDate: DateTime.now(),
+        firstDate: DateTime(2020),
+        lastDate: DateTime
+          (3000))
+        .then((pickedDate) {
+      if (pickedDate == null) {
+        return;
+      }
+      setState(() {
+        dateController.text = formatter.format(pickedDate).toString();
+      });
+    });
   }
 }
