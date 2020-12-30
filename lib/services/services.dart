afec0565bfacc877572ca9cc329227324ab44985
anthonychibuike <anthonychibuike246@gmail.com>
2020-12-30 04:01:32 +0100

    setup flutter local notification

---
+++ lib/services/services.dart
@@ -0,0 +1,29 @@
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+class LocalNotification {
+  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+
+  Future<void> showNotification({DateTime time,String title,String body})async{
+
+    var initializationSettingsAndroid = AndroidInitializationSettings('ic_launcher');
+    var initializationSettingsIOS = IOSInitializationSettings();
+    var initializationSettings = InitializationSettings(
+        initializationSettingsAndroid, initializationSettingsIOS);
+    await flutterLocalNotificationsPlugin.initialize(initializationSettings);
+
+    var androidPlatformChannelSpecifics =
+    AndroidNotificationDetails('your other channel id',
+        'your other channel name', 'your other channel description');
+    var iOSPlatformChannelSpecifics =
+    IOSNotificationDetails();
+    NotificationDetails platformChannelSpecifics = NotificationDetails(
+        androidPlatformChannelSpecifics, iOSPlatformChannelSpecifics);
+    await flutterLocalNotificationsPlugin.schedule(
+        0,
+        title,
+        body,
+        time,
+        platformChannelSpecifics);
+
+  }
+}
